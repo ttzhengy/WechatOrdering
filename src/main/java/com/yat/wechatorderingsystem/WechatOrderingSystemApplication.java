2e66685fe4f8e2c6ca82666b90d92085fee8f191
yat <303980015@qq.com>
2021-05-18 15:58:10 +0800

    新建项目

---
+++ src/main/java/com/yat/wechatorderingsystem/WechatOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yat.wechatorderingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatOrderingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatOrderingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yat/wechatorderingsystem/WechatOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yat.wechatorderingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatOrderingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
