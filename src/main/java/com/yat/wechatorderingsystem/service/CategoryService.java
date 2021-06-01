24fcfaccb38c07fff59032cd1e30d81c903ab5b8
yat <303980015@qq.com>
2021-06-01 15:35:52 +0800

    商品类目service接口及实现类

--- src/main/java/com/yat/wechatorderingsystem/service/CategoryService.java
+++ src/main/java/com/yat/wechatorderingsystem/service/CategoryService.java
@@ -15,7 +15,9 @@ public interface CategoryService {
     // 根据类目类型查询多个类目
     List<ProductCategory> selectByTypeIn(List<Integer> type);
 
+    // 增加商品类目
     ProductCategory insert(ProductCategory productCategory);
 
+    // 修改商品类目
     ProductCategory update(ProductCategory productCategory);
 }
