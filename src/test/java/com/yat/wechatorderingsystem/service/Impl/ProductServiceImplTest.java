5d4d2f624a5f71354792881ea65d590469f31224
yat <303980015@qq.com>
2021-06-02 17:10:35 +0800

    商品信息Service实现类

--- src/test/java/com/yat/wechatorderingsystem/service/Impl/ProductServiceImplTest.java
+++ src/test/java/com/yat/wechatorderingsystem/service/Impl/ProductServiceImplTest.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
