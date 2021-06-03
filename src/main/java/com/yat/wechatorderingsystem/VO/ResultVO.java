12f1fd78c0996e05f2ed919cd48f0196a19742f0
yat <303980015@qq.com>
2021-06-03 16:03:30 +0800

    最外层的视图对象

--- src/main/java/com/yat/wechatorderingsystem/VO/ResultVO.java
+++ src/main/java/com/yat/wechatorderingsystem/VO/ResultVO.java
@@ -1,8 +1,10 @@
 package com.yat.wechatorderingsystem.VO;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+// @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResultVO<T> {
 
     // 错误码
